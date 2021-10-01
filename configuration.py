a4f6979fab3c2299a04ae8f0b921471bf92eea04
Anchal Sinha <anchalsinha@Anchals-MacBook-Pro.local>
2021-09-30 17:29:14 -0700

    Implementation of 'O' and function stubs for simulator

--- configuration.py
+++ configuration.py
@@ -8,4 +8,15 @@ A = np.array([[-1, 0], [1, 0], [0, -1], [0, 1], [0, 0]])
 
 # define P and O
 P = None
-O = None
+
+O = {}
+Rd_loc = [2, 2]
+Rs_loc = [4, 2]
+def output(state):
+  dD = np.linalg.norm(state - Rd_loc)
+  dS = np.linalg.norm(state - Rs_loc)
+  h = 2/((1/dD) + (1/dS))
+  o = [[np.ceil(h), 1 - (np.ceil(h) - h)], [np.floor(h), np.floor(h) - h]]
+
+for s in S:
+    O[s] = output(s)
--- simulator.py
+++ simulator.py
@@ -13,16 +13,12 @@ grid = np.array([
 Rd_loc = list(zip(*np.where(grid == 'D')))[0]
 Rs_loc = list(zip(*np.where(grid == 'S')))[0]
 
-# def output(current state, grid)
-#  return output probabilities
-def output(state):
-  dD = np.linalg.norm(state - Rd_loc)
-  dS = np.linalg.norm(state - Rs_loc)
-  h = 2/((1/dD) + (1/dS))
-  o = [[np.ceil(h), 1 - (np.ceil(h) - h)], [np.floor(h), np.floor(h) - h]]
-  
+state = [0, 2]
 
-# def transition(current state, grid, action)
-#  return next state
+def selectAction():
+    pass
+
+def takeAction(action):
+    pass
 
 # main function for visuals/logging
