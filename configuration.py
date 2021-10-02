456acd0e02bdf0f4fc3fe53437d871135a024138
Anchal Sinha <anchalsinha@Anchals-MacBook-Pro.local>
2021-10-02 11:29:33 -0700

    Integration of transition probabilities and improved visuals

--- configuration.py
+++ configuration.py
@@ -9,13 +9,9 @@ grid = np.array([
 # define state and action spaces
 rows = 5
 cols = 5
-#S = np.array([(x, y) for x in range(0, rows) for y in range(0, cols)])
-#A = np.array([(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)])
-S = [(x, y) for x in range(0, rows) for y in range(0, cols)]
-A = [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)]
+S = [(y, x) for y in range(0, rows) for x in range(0, cols)]
+A = [(0, -1), (0, 1), (-1, 0), (1, 0), (0, 0)]
 
-
-A_indices = [1,2,3,4,0]
 # define P and O
 P = 0.3
 
@@ -31,58 +27,37 @@ def output(state):
 # for s in S:
 #     O[s] = output(s)
 transition_matrix = {}
-def convert_index(i,j):
-    return int( i*rows + j)
 
 def possible_jumps(present_state):
     possible_jumps= []
     for a in A:
         jump_x = present_state[0] +a[0]
         jump_y = present_state[1] + a[1]
-        print(jump_x, jump_y)
         if jump_x >= 0 and jump_x < rows and jump_y >= 0 and jump_y < cols:
             if grid[jump_x, jump_y] == '0' or grid[jump_x, jump_y] == 'S' or grid[jump_x, jump_y] == 'D':
-                possible_jumps.append([jump_x, jump_y])
+                possible_jumps.append((jump_x, jump_y))
     return possible_jumps
 
 def transition_matrix():
     # The states are represented from 1 to rows*cols using
     transition_mat = {}
     for s in S:
-        sindex = convert_index(s[0], s[1])
-        print( 's ', s,' sindex ', sindex)
-
-        list_possible_jumps=possible_jumps(s)
-        possible_jump_indices = []
-        for jump in list_possible_jumps:
-            possible_jump_indices.append(convert_index(jump[0], jump[1]))
+        list_possible_jumps = possible_jumps(s)
         a_dic ={}
-        print("possible jumps",possible_jump_indices)
-        for a in A_indices:
-            s_dict={}
-            #print('inidces of A',A_indices.index(a), 'a is ',a)
-            desired_state = (s[0] + A[A_indices.index(a)][0], s[1] +A[A_indices.index(a)][1])
-            desired_state_index = convert_index(desired_state[0], desired_state[1])
+
+        for a in A:
+            s_dict = {}
+            desired_state = s + a
 
             for s_ in S:
-                s_index = convert_index(s_[0], s_[1])
-                if desired_state_index == s_index and s_index in possible_jump_indices :
-                    print("added for the 1-p", s_index, " for action ", a)
-                    s_dict[s_index] =  float(1-P)
-
-                elif s_index in possible_jump_indices and s_index not in s_dict:
-                    print("added present index p/4", s_index)
-                    s_dict[s_index] = P/4.0
+                if desired_state == s_ and s_ in list_possible_jumps:
+                    s_dict[s_] = float(1-P)
+                elif s_ in list_possible_jumps and s_ not in s_dict:
+                    s_dict[s_] = P/4.0
                 else:
-                    s_dict[s_index] =0
+                    s_dict[s_] = 0
             a_dic[a]= s_dict
-        transition_mat[sindex]  = a_dic
+        transition_mat[tuple(s)]  = a_dic
     return transition_mat
 
-trans = transition_matrix()
-test_index_s = (1,0)
-test_index_s_ = (0,0)
-action = (1,0)
-#print(trans[20][3])
-## directly give the input interms of (s,a,s')
-print(trans[convert_index(test_index_s[0], test_index_s[1])][A_indices[A.index(action)]][convert_index(test_index_s_[0], test_index_s_[1])])
+P = transition_matrix()
--- simulator.py
+++ simulator.py
@@ -6,6 +6,8 @@ import sys
 
 from configuration import S, A, P, O
 
+quitKey = 0
+
 # define grid
 grid = np.array([
         [0, 0, 0, 0, 1],
@@ -19,41 +21,70 @@ Rs_loc = list(zip(*np.where(grid == 'S')))[0]
 
 state = [0, 2]
 
-def moveToNextState():
-    actions_dict = P[state]
-    actions = actions_dict.keys()
-    for action in actions:
-        transition = actions[action]
-        next_state = np.random.choice(transition.keys(), p=transition.values())
-        print(f'Next state: {next_state}, Probability of reaching this state: {transition[next_state]}')
-    
+# def moveToNextState(action):
+#     global state
+#     actions = P[tuple(state)]
 
+#     transition = actions[action]
+#     states = transition.keys()
+#     next_state = np.random.choice(list(states), p=list(transition.values()))
+#     print(f'Next state: {next_state}, Probability of reaching this state: {transition[next_state]}')
+#     state = next_state
+#     print(f'Output: {O[tuple(state)]}')
+#     drawGridworld()
+
+def moveToNextState(action):
+    global state
+    next_state = np.add(state, action)
+    print(f'Next state: {next_state}')
+    state = next_state
+    # print(f'Output: {O[tuple(state)]}')
+    drawGridworld()
+    
+def drawGridworld(wall_char='X', state_char='O'):
+    for i in range(len(grid)):
+        row = '|'
+        for j in range(len(grid[0])):
+            if i == state[0] and j == state[1]:
+                row += f'{state_char}|'
+                continue
+
+            if grid[i, j] == '0':
+                row += '_|'
+            elif grid[i, j] == '1':
+                row += f'{wall_char}|'
+            else:
+                row += f'{grid[i, j]}|'
+        print(row)
+
+    print('\n\n\n')
 
 def takeAction(key):
+    global quitKey
+
     if key == keyboard.Key.left:
         action = A[0]
     elif key == keyboard.Key.right:
         action = A[1]
-    elif key == keyboard.Key.down:
-        action = A[2]
     elif key == keyboard.Key.up:
+        action = A[2]
+    elif key == keyboard.Key.down:
         action = A[3]
     elif key == keyboard.Key.space:
         action = A[4]
-    elif key == keyboard.Key.q:
-        sys.exit(0)
-    else:
-        print('Invalid key pressed')
+    elif key.char == 'q':
+        quitKey = 1
         return
-
+    else:
+        print(f'Invalid key pressed: {key}')
+        return
     
     print(f'Action: {action}')
+    moveToNextState(action)
 
 listener = keyboard.Listener(on_press=takeAction)
 listener.start()
 
-
-
 if __name__ == '__main__':
-    while True:
+    while not quitKey:
         time.sleep(1)
