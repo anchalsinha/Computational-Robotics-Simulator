c7b792fbb922a86045e2c63d34275f5508f0e10b
Alexander Swerdlow <aswerdlow1@gmail.com>
2021-10-07 12:11:18 -0700

    Add gitignore, fix mdp for gridworld

--- gridworld.py
+++ gridworld.py
@@ -2,17 +2,29 @@ import numpy as np
 
 from environment import GridworldEnvironment
 from simulator import GridworldSimulator
+from mdp import MDP
 
 grid = np.array([
-        [0, 0, 0, 0, 1],
-        [0, 1, 1, 0, 1],
-        [0, 0, 'D', 0, 1],
-        [0, 1, 1, 0, 1],
-        [0, 0, 'S', 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 1, 1, 0, 1],
+    [0, 0, 'D', 0, 1],
+    [0, 1, 1, 0, 1],
+    [0, 0, 'S', 0, 1],
 ])
 Pe = 0.3
 initial_state = (0, 2)
 
 gridworld_environment = GridworldEnvironment(grid, Pe)
+
+def reward(state, action, next_state):
+    if next_state == (2, 2) or next_state == (4, 2):
+        return 1
+    else:
+        return 0
+
+gridworld_mdp = MDP(gridworld_environment, reward)
+#gridworld_policy = gridworld_mdp.value_iteration(0.01, 0.5)
+gridworld_policy = gridworld_mdp.policy_iteration(0.01, 0.5)
+
 gridworld_simulator = GridworldSimulator(gridworld_environment, initial_state)
-gridworld_simulator.run_manual_input()
+gridworld_simulator.run_policy(gridworld_policy)
--- mdp/policy.py
+++ mdp/policy.py
@@ -1,18 +1,17 @@
-from .base_environment import Environment
 import numpy as np
-from collections import defaultdict
 from abc import ABC, abstractmethod
 
-
 class MDP(ABC):
-    def __init__(self, environment):
-        self.environment = environment
-
+    def __init__(self, environment, reward):
+        self.state = environment.S
+        self.action = environment.A
+        self.p = environment.P
+        self.r = reward
         self.exitProgram = 0
 
-
-    def value_iteration(self, termination_epsilon = 0.01):
-        policy, value, value_delta, termination_epsilon = {}, {}, 0
+    # Doesn't work when gamma = 1.0
+    def value_iteration(self, termination_epsilon = 0.01, gamma = 0.5):
+        policy, value, value_delta = {}, {}, 1
         while value_delta > termination_epsilon:
             value_delta = 0
             for state in self.state:
@@ -20,22 +19,23 @@ class MDP(ABC):
                 for action in self.action:
                     action_value = 0
                     for next_state in self.state:
-                        movement_prob = self.p(state, action, next_state)
+                        movement_prob = self.p.get(state, {}).get(action, {}).get(next_state, 0)
                         movement_reward = self.r(state, action, next_state)
-                        action_value += movement_prob * (movement_reward + self.gamma * value[next_state])
+                        action_value += movement_prob * (movement_reward + gamma * value.get(next_state, 0))
                     
                     if best_action is None or action_value > best_value:
                         best_action, best_value = action, action_value
 
-                value_delta = max(value_delta, abs(value[state] - best_value))
+                value_delta = max(value_delta, abs(value.get(state, 0) - best_value))
                 policy[state], value[state] = best_action, best_value
-                
-    def policy_iteration(self, termination_epsilon = 0.01):
-        policy, value, value_delta  = {}, {}, 0
+        return policy
+        
+    def policy_iteration(self, termination_epsilon = 0.01, gamma = 0.5):
+        policy, value, value_delta  = {}, {}, 1
 
         # Randomly initialize policy
         for state in self.state:
-            policy[state] = np.random.choice(self.action)
+            policy[state] = self.action[np.random.choice(range(len(self.action)), 1)[0]]
 
         while True:
             # Policy Evaluation
@@ -44,11 +44,11 @@ class MDP(ABC):
                 for state in self.state:
                     state_value = 0
                     for next_state in self.state:
-                        movement_prob = self.p(state, policy[state], next_state)
+                        movement_prob = self.p.get(state, {}).get(policy[state], {}).get(next_state, 0)
                         movement_reward = self.r(state, policy[state], next_state)
-                        state_value += movement_prob * (movement_reward + self.gamma * value[next_state])
+                        state_value += movement_prob * (movement_reward + gamma * value.get(next_state, 0))
 
-                    value_delta = max(value_delta, abs(value[state] - state_value))
+                    value_delta = max(value_delta, abs(value.get(state, 0) - state_value))
                     value[state] = state_value
 
             # Policy Improvement
@@ -58,9 +58,9 @@ class MDP(ABC):
                 for action in self.action:
                     action_value = 0
                     for next_state in self.state:
-                        movement_prob = self.p(state, action, next_state)
+                        movement_prob = self.p.get(state, {}).get(action, {}).get(next_state, 0)
                         movement_reward = self.r(state, action, next_state)
-                        action_value += movement_prob * (movement_reward + self.gamma * value[next_state])
+                        action_value += movement_prob * (movement_reward + gamma * value.get(next_state, 0))
 
                     if best_action is None or action_value > best_value:
                         best_action, best_value = action, action_value
@@ -74,4 +74,4 @@ class MDP(ABC):
 
             if is_policy_stable:
                 break
-                
+        return policy
