2e1a6568587c03e96afc967c3b6a6c04b29c83cd
anwxwna <anwchatto@gmail.com>
2021-10-11 01:01:51 -0700

    nline viz

--- simulator/number_line_simulator.py
+++ simulator/number_line_simulator.py
@@ -1,11 +1,49 @@
 import numpy as np
 import time
-
+from abc import ABC, abstractmethod
+from pynput import keyboard
 from .base_simulator import Simulator
 
 class NumberlineSimulator(Simulator):
     # TODO: Implement this function
     def visualize(self):
-        pass
-    def run(self):
-        pass
+        nline = self.environment.position_space
+        for i in range(len(nline)):
+            if i == self.state[0]:
+                print("O")
+            else:
+                print("-")
+            
+        print('\n\n\n')
+
+    def keyboardCallback(self, key):
+        '''
+        Keyboard listener function to select an action based on key input
+        '''
+        A = self.environment.A
+
+        try:
+            if key == keyboard.Key.left:
+                action = A[0]
+            elif key == keyboard.Key.right:
+                action = A[1]
+            elif key == keyboard.Key.up:
+                action = A[2]
+            elif key.char == 'q':
+                self.exitProgram = 1
+                return
+            self.nextStep(action)
+        except:
+            pass
+
+    def run_manual_input(self):
+        '''
+        Simulation loop
+        '''
+        # keyboard listener in background thread
+        listener = keyboard.Listener(on_press=self.keyboardCallback)
+        listener.start()
+
+        self.visualize()
+        while not self.exitProgram:
+            time.sleep(0.1)
