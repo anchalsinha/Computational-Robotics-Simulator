6163663fbe268901bf0daa281e9d229d6d7baa9b
Anchal Sinha <anchalsinha@ucla.edu>
2021-10-14 17:45:55 -0700

    bug fix

--- environment/base_environment.py
+++ environment/base_environment.py
@@ -1,14 +1,13 @@
 from abc import ABC, abstractmethod
 
 class Environment(ABC):
-    def __init__(self, S, A, P, O, R, functions):
+    def __init__(self, S, A, P, O, R):
         self.S, self.A, self.P, self.O, self.R = S, A, P, O, R
-        self.basis_functions = functions
     
     @abstractmethod
     def calculate_phi(self, state):
         pass
-    
+
     @abstractmethod
     def visualize(self, states):
         pass
