285be7f855101cc384c6e29b3634dae9fefd77b5
Anchal Sinha <anchalsinha@ucla.edu>
2021-10-19 17:13:37 -0700

    chessboard motion planning files

---
+++ motion_planning/chessboard.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class ChessboardEnvironment:
+    def __init__(self, board):
+        '''
+        Define discrete state space system configuration from the defined grid. In the grid array,
+        walls are defined as '1', empty spaces as '0'
+        '''
+        self.board = board
+
+        self.obstacle_coords = np.array(np.where(board == 1)).T
+        self.rows, self.cols = board.shape
+
+        # define state and action spaces
+        S = [(y, x) for y in range(0, self.rows) for x in range(0, self.cols)]
+        A = [(2, -1), (2, 1), (-2, -1), (-2, 1), (1, -2), (1, 2), (-1, -2), (-1, 2)]
+
+
+
+    def get_vertices(self, S, A):
+        pass
+
+    def get_edges(self, S):
+        pass
+
+    def possible_jumps(self, A, present_state):
+        '''
+        List all possible movements from the current state as to not select a wall or boundary as the 
+        next state.
+        '''
+        possible_jumps= []
+        for a in A:
+            jump_y = present_state[0] + a[0]
+            jump_x = present_state[1] + a[1]
+            if jump_y >= 0 and jump_y < self.rows and jump_x >= 0 and jump_x < self.cols:
+                if self.grid[jump_y, jump_x] != '1' and (self.grid[jump_y, jump_x] == '0' or np.array([jump_y, jump_x]) in self.target_coords):
+                    possible_jumps.append((jump_y, jump_x))
+        return possible_jumps
+
+    
