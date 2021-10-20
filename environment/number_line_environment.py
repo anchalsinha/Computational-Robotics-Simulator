9125d80f6b07c488cebebbba98702f6f5bf12813
Bopp1312 <jonathanbopp@ucla.edu>
2021-10-19 17:51:51 -0700

    Fixed discretization bug

--- environment/number_line_environment.py
+++ environment/number_line_environment.py
@@ -25,7 +25,7 @@ class NumberLineEnvironment(Environment):
         self.position = 0  # position
         self.velocity = 0  # velocity
         self.time = 0
-        
+
         self.target_state = target_state
 
 
@@ -36,15 +36,17 @@ class NumberLineEnvironment(Environment):
         S = [(y, x) for y in self.position_space for x in  self.velocity_space]  # set of all states
         A = [-1,0,1]                                   # set of all actions
 
-        O = self.calculate_observation_set(S)            # set of all observations 
+        O = self.calculate_observation_set(S)            # set of all observations
         P = self.calculate_transition_prob_set(S, A)    # set of all transtions probabilities
         R = self.calculate_reward_set(S,A)
 
         Environment.__init__(self, S, A, P, O,R)
-    
+
     def state_space_discretization(self,resolution= 0.1):
-        self.position_space = np.array([a for a in range(-self.y_max,self.y_max,resolution)])
-        self.velocity_space = np.array([a for a in range(-self.v_max,self.v_max,resolution)])
+        position_resolution = int(2.0*self.y_max/resolution)
+        velocity_resolution = int(2.0*self.v_max/resolution)
+        self.position_space = np.array([a for a in range(-self.y_max,self.y_max,position_resolution)])
+        self.velocity_space = np.array([a for a in range(-self.v_max,self.v_max,velocity_resolution)])
 
     def get_p(self,current_state, action, next_state):
         ##TODO add the prob determination via knn here
@@ -52,14 +54,14 @@ class NumberLineEnvironment(Environment):
 
     def get_r(self,current_state, action, next_state):
         return self.R[current_state][action][next_state]
-    
+
     def action_space_discretization(self):
         pass
 
     def phi(self,y) -> int :
         """potential field"""
         return self.hill_size*np.sin((2*np.pi*y)/self.y_max)
-    
+
     def sim_input(self, t):
         return 1
 
@@ -74,7 +76,7 @@ class NumberLineEnvironment(Environment):
         - 1 for next state desired state
         - v-1 for next state undesired state
         """
-        
+
         R = {}
         for state in S:
             for action in A:
@@ -85,7 +87,7 @@ class NumberLineEnvironment(Environment):
                         R[state][action][next_state] = 1
                     elif next_state != self.target_state:
                         R[state][action][next_state] = -1
-                    
+
         return R
 
     def calculate_observation_set(self,S) :
@@ -120,7 +122,7 @@ class NumberLineEnvironment(Environment):
     def _next_velocity(self,state,action):
         next_velocity = state[1] + (1 / self.m) * self._net_force(action) + self._noise_dynamics(state[1])
         return self._check_bounds(next_velocity,self.v_max)
-    
+
     def calculate_transition_prob_set(self,S, A):
         transition_mat = {}
         for state in S: #current State
@@ -128,7 +130,7 @@ class NumberLineEnvironment(Environment):
             a_dict ={}
             for action in A:  #current action
                 s_dict = {}
-                
+
                 if has_crashed:
                     next_velocity = 0
                 else:
@@ -137,41 +139,41 @@ class NumberLineEnvironment(Environment):
                 next_state= (next_position,next_velocity)
                 s_dict[next_state] = 1 #transition probability 1 for now
                 # calculate prob here, from the output of the sys dynamincs equation using it with the probability bins (another function) to calculate p, using knn
-            
+
                 a_dict[action] = s_dict
             transition_mat[tuple(state)] = a_dict
         return transition_mat
-                
+
     def _probability_of_state(self):
         pass
-    
+
     def _knn(self):
         pass
 
     def possible_actions(self, present_state):
         ##TODO fit to numberline problem
         return self.A
-    
+
 
     def _net_force(self, input_force) -> int:
         f_net = int(input_force + derivative(self.phi, self.state[0]) )
         return f_net
-    
+
     def _noise_sensor(self,v):
         return np.random.normal(0, np.abs(0.5*v))
-    
+
     def _noise_dynamics(self,v):
         """
         Speed wobble dynamics
         """
-        #TODO change to new speed wobble 
+        #TODO change to new speed wobble
         return np.random.normal(0, np.abs(0.1*v))
-    
+
     def possible_actions(self):
         return self.A
-    
+
     ## PRM ###############
-    ##--------------------------------------------------------- 
+    ##---------------------------------------------------------
     # Two points in state space can be connected if there exists a constant input fi that drives the initial state to the final state (over some upper-bounded duration of time)
 
     def PRM(self):
@@ -188,9 +190,3 @@ class NumberLineEnvironment(Environment):
             False is not
 
         """
-
-
-
-
-
-   
